11925da90680b9abb55f5fbe29a179cb9bbfe536
espadv69 <aitorjob69@gmail.com>
2025-01-19 19:27:43 +0100

    Refactor RecipeCard component for improved structure and styling

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -2,11 +2,17 @@ import '../css/RecipeCard.css'
 
 const RecipeCard = ({ recipe }) => {
   return (
-    <div>
-      <h3 className="recipe-card">{recipe.name}</h3>
-      <p>Ingredients: {recipe.ingredients.split(',').join(', ')}</p>
-      <p>Steps: {recipe.steps}</p>
-      <p>Difficulty: {recipe.difficulty}</p>
+    <div className="recipe-card-container">
+      <h3 className="recipe-card-name">{recipe.name}</h3>
+      <p className="recipe-card-ingredients">
+        Ingredients: <span>{recipe.ingredients.split(',').join(', ')}</span>
+      </p>
+      <p className="recipe-card-steps">
+        Steps: <span>{recipe.steps}</span>
+      </p>
+      <p className="recipe-card-difficulty">
+        Difficulty: <span>{recipe.difficulty}</span>
+      </p>
     </div>
   )
 }
