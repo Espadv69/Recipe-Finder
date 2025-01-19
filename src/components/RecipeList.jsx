a25e27b76b5b9992541a1ffcc5af0a348a8bb1f1
espadv69 <aitorjob69@gmail.com>
2025-01-19 19:08:13 +0100

    Add message for empty recipe list in RecipeList component

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,6 +1,10 @@
 import RecipeCard from './RecipeCard.jsx'
 
 const RecipeList = ({ recipes }) => {
+  if (!recipes || recipes.length === 0) {
+    return <p>No recipes found!</p> // Mensaje para cuando no hay recetas
+  }
+  
   return (
     <div>
       {recipes.map((recipe, index) => (
