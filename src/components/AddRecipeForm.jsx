703e6f758e833e56f6b91d2e00e0c6d10b232621
espadv69 <aitorjob69@gmail.com>
2025-01-19 19:08:17 +0100

    Refactor AddRecipeForm to accept onAddRecipe prop for improved recipe handling

--- src/components/AddRecipeForm.jsx
+++ src/components/AddRecipeForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const AddRecipeForm = () => {
+const AddRecipeForm = ({ onAddRecipe }) => {
   const [recipe, setRecipe] = useState({
     name: '',
     ingredients: '',
