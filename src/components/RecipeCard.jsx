b2f1f877708183ca8ce594122051a42d8db190b9
espadv69 <aitorjob69@gmail.com>
2025-01-19 19:08:09 +0100

    Enhance RecipeCard component with additional recipe details and styling

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,9 +1,12 @@
+import '../css/RecipeCard.css'
+
 const RecipeCard = ({ recipe }) => {
   return (
     <div>
-      <h3>{recipe.name}</h3>
-      <p>Difficulty: {recipe.difficulty}</p>
+      <h3 className="recipe-card">{recipe.name}</h3>
       <p>Ingredients: {recipe.ingredients.split(',').join(', ')}</p>
+      <p>Steps: {recipe.steps}</p>
+      <p>Difficulty: {recipe.difficulty}</p>
     </div>
   )
 }
