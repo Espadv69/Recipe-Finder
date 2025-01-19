85a09bd297d24e8c3668537f8eba6934c3ff8b33
espadv69 <aitorjob69@gmail.com>
2025-01-19 18:49:12 +0100

    Implement recipe management in App component with state and handlers

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import { useState } from 'react'
+
+import { AddRecipeForm } from './components/AddRecipeForm.jsx'
+import { RecipeList } from './components/RecipeList.jsx'
+
 export const App = () => {
-  
+  const [recipes, setRecipes] = useState([])
+
+  const handleAddRecipe = (newRecipe) => {
+    setRecipes([...recipes, newRecipe])
+  }
+
+  return (
+    <div>
+      <h1>Recipe Finder</h1>
+      <AddRecipeForm onAddRecipe={handleAddRecipe} />
+      <RecipeList recipes={recipes} />
+    </div>
+  )
 }
